3048319debb50c79a4ac21d617b6480d2991b006
tristan-nouzille <tristan.nouzille-77@hotmail.fr>
2024-10-31 18:58:50 +0100

    comprendre le javaScript et les id

--- frontend/config/api-config.js
+++ frontend/config/api-config.js
@@ -1,6 +1,4 @@
-function requeteApi(){
-    fetch("https://localhost:8000/api/v1/titles/9008642?format=api")
-   .then((response) => response.json())
-   .then((json) => console.log(json));
-}
-console.log(requeteApi)
+
+
+
+
